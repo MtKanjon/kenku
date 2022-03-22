2ccd98df646ae00d1a47cc450fd432a139d9bbc3
MtKanjon <mt@kanjon.art>
2022-03-21 18:35:18 -0700

    Add wide command

---
+++ cogs/crow/__init__.py
@@ -0,0 +1,4 @@
+from .crow import Crow
+
+def setup(bot):
+    bot.add_cog(Crow(bot))
---
+++ cogs/crow/crow.py
@@ -0,0 +1,28 @@
+from io import BytesIO
+
+import discord
+
+from PIL import Image
+from redbot.core import commands
+
+class Crow(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    WIDE_HEIGHT = 48
+    WIDE_WIDTH_MUL = 3
+
+    @commands.command()
+    async def wide(self, ctx, emoji: discord.Emoji):
+        emoji_data = BytesIO(await emoji.url.read())
+
+        resized_file = BytesIO()
+        with Image.open(emoji_data) as img:
+            img_resized = img.resize((self.WIDE_HEIGHT * self.WIDE_WIDTH_MUL, self.WIDE_HEIGHT))
+            img_png = img_resized.save(resized_file, format="PNG")
+
+        resized_file.seek(0)
+        file = discord.File(resized_file, filename=f"{emoji.name}_wide.png")
+        sent = await ctx.send(file=file)
+        await sent.add_reaction(emoji)
+
