8c44268c887250c54ec98b8996ac05b1f738c121
MtKanjon <mt@kanjon.art>
2022-03-23 18:13:05 -0700

    Allow variable width on wide

--- cogs/crow/crow.py
+++ cogs/crow/crow.py
@@ -5,23 +5,28 @@ import discord
 from PIL import Image
 from redbot.core import commands
 
+WIDE_HEIGHT = 48
+
 class Crow(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    WIDE_HEIGHT = 48
-    WIDE_WIDTH_MUL = 3
 
     @commands.command()
-    async def wide(self, ctx, emoji: discord.PartialEmoji):
+    async def wide(self, ctx, emoji: discord.PartialEmoji, size: int = 3):
+        if size < 2 or size > 10:
+            await ctx.react_quietly("🚷")
+            return
+
         emoji_data = BytesIO(await emoji.url.read())
-
-        resized_file = BytesIO()
-        with Image.open(emoji_data) as img:
-            img_resized = img.resize((self.WIDE_HEIGHT * self.WIDE_WIDTH_MUL, self.WIDE_HEIGHT))
-            img_png = img_resized.save(resized_file, format="PNG")
-
-        resized_file.seek(0)
+        resized_file = self.resize_image(emoji_data, WIDE_HEIGHT * size, WIDE_HEIGHT)
         file = discord.File(resized_file, filename=f"{emoji.name}_wide.png")
         sent = await ctx.send(file=file)
+    
+    def resize_image(self, image_data: BytesIO, width: int, height: int):
+        out = BytesIO()
+        with Image.open(image_data) as img:
+            resized = img.resize((width, height))
+            resized.save(out, format="PNG")
+        out.seek(0)
+        return out
 
